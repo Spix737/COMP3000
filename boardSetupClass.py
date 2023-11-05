595f832236660bd5dda25845f1d3abd787f0c7c2
Spyros Babalitis <spyridon.babalitis@students.plymouth.ac.uk>
2023-11-05 01:16:25 +0000

    Board Setup Progress
    Completed:
    Balanced resource tile placement
    Display board function

--- boardSetupClass.py
+++ boardSetupClass.py
@@ -1,27 +1,18 @@
 class Board:
     def __init__(self):
-        board = [[]] * 182
-        # 16
-        # 16
-            # 19
-            # 19
+        self.board = [[]] * 183
+        # 13
+        # 13
+            # 17
+            # 17
                 # 21
                 # 21
                 # 21
-            # 19
-            # 19
-        # 16
-        # 16
+            # 17
+            # 17
+        # 13
+        # 13
 
-    vertices = [
-        0,2,4,6,8,10,12, #1top
-        26,28,30,32,34,36,38,40,42, #2top, 1bot
-        60,62,64,66,68,70,72,74,76,78,80, #3top, 2bot
-        102,104,106,108,110,112,114,116,118,120,122, #4top, 3bot
-        140,142,144,146,148,150,152,154,156,158, #5top, 4bot
-        170,172,174,176,178,180,182 #5bot
-        ]
-    
     # array of values which award a resource when rolled
     probability_array = [2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12]
 
@@ -33,7 +24,17 @@ class Board:
         'm','m','m','m',
         'h','h','h',
         'p','p','p',
+        # ]
         'X']
+    
+    vertices = [
+        0,2,4,6,8,10,12, #1top
+        26,28,30,32,34,36,38,40,42, #2top, 1bot
+        60,62,64,66,68,70,72,74,76,78,80, #3top, 2bot
+        102,104,106,108,110,112,114,116,118,120,122, #4top, 3bot
+        140,142,144,146,148,150,152,154,156, #5top, 4bot
+        170,172,174,176,178,180,182 #5bot
+        ]
 
     edges = [
         1,3,5,7,9,11,
@@ -49,8 +50,78 @@ class Board:
         171,173,175,177,179,181
     ]
 
+    tile_value_left = [
+            14,18,22, #tileRow1
+          44,48,52,56, #tileRow2
+        82,86,90,94,98, #tileRow3
+          124,128,132,136, #tileRow4
+            158,162,166 #tileRow5
+    ]
+
+    tile_types_center = [
+            15,19,23, #tileRow1
+          45,49,53,57, #tileRow2
+        83,87,91,95,99, #tileRow3
+          125,129,133,137, #tileRow4
+           159,163,167 #tileRow5
+    ]
+
+    is_robber_right = [
+            16,20,24, #tileRow1
+          46,50,54,58, #tileRow2
+        84,88,92,96,100, #tileRow3
+          126,130,134,138, #tileRow4
+            160,164,168 #tileRow5
+    ]
+
     #to traverse to other hexes, add 4 * number of hexes to traverse to each value
     first_hex = [0, 1, 2, 3, 4,
                 13,14,15,16,17,
                 28,29,30,31,32]
         
+    perimeter_vertices = [
+        0,2,4,6,8,10,12, #1top
+        26,28,30,32,34,36,38,40,42, #2top, 1bot
+        60,62,64,66,68,70,72,74,76,78,80, #3top, 2bot
+        102,104,106,108,110,112,114,116,118,120,122, #4top, 3bot
+        140,142,144,146,148,150,152,154,156, #5top, 4bot
+        170,172,174,176,178,180,182 #5bot
+        ]
+    
+    ports_vertices = [
+        0,2,
+        6,8,
+        26,62,
+        40,42,
+        80,122,
+        104,140,
+        154,156,
+        170,172,
+        176,178
+    ]
+
+    port_types = [
+        'wh',
+        'wo',
+        'or',
+        'cl',
+        'sh',
+        '3',
+        '3',
+        '3',
+        '3',
+    ]
+
+    def display_board(self):
+        # print(self.board[0:13])
+        print(self.board[13:26])
+        # print(self.board[26:43])
+        print(self.board[43:60])
+        # print(self.board[60:81])
+        print(self.board[81:102])
+        # print(self.board[102:123])
+        print(self.board[123:140])
+        # print(self.board[140:157])
+        print(self.board[157:174])
+        # print(self.board[174:183])
+    
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from boardSetupClass import Board 
 from PlayerClass import Player
 from GameLogic import roll_dice
+import random
 
 def setup_game():
-    board = Board()
+    setup_players()
+    setup_board()
+
+
+def setup_players():
     players = [Player("Player 1"), Player("Player 2"), Player("Player 3"), Player("Player 4")]
     # ... set up tiles, resources, players, etc.
     # Determine the initial order based on dice rolls
@@ -22,12 +27,100 @@ def setup_game():
 
     # Find the index of the first player in the list of players
     first_player_index = players.index(first_player)
-    
+
     # Determine the clockwise order of players
     player_order = players[first_player_index:] + players[:first_player_index]
 
     # Now, player_order contains the order in which players will take their turns, with the first player going first in a clockwise direction.
     print("First Player:", first_player.player_name)
     print("Player Order:", [player.player_name for player in player_order])
+
+
+def setup_board():
+    def place_and_check(i, first = None, second = None, third = None):
+        x = True
+        err_count = 0
+        unusable = []
+        while x == True:
+            ptile = random.choice(board.terrain_array)
+            if third == None:
+                if second == None:
+                    if(first == None):
+                        board.board[board.tile_types_center[i]] = ptile
+                        board.terrain_array.remove(ptile)
+                        x = False
+                    else:
+                        if(ptile in unusable
+                            or board.board[board.tile_types_center[i] - first] == ptile):
+                            unusable.append(ptile)
+                            err_count += 1
+                        else:
+                            board.board[board.tile_types_center[i]] = ptile
+                            board.terrain_array.remove(ptile)
+                            x = False
+                else:
+                    if(ptile in unusable
+                        or board.board[board.tile_types_center[i] - first] == ptile
+                        or board.board[board.tile_types_center[i] - second] == ptile):
+                        err_count += 1
+                        unusable.append(ptile)
+                    else:
+                        board.board[board.tile_types_center[i]] = ptile
+                        board.terrain_array.remove(ptile)
+                        x = False
+            else:
+                if(ptile in unusable
+                    or board.board[board.tile_types_center[i] - first] == ptile
+                    or board.board[board.tile_types_center[i] - second] == ptile
+                    or board.board[board.tile_types_center[i] - third] == ptile):
+                    unusable.append(ptile)
+                    err_count += 1
+                else:
+                    board.board[board.tile_types_center[i]] = ptile
+                    board.terrain_array.remove(ptile)
+                    x = False
+            if err_count > 20:
+                print("Error: too many iterations")
+                break
+
+    board = Board()
+
+    # Shuffle terrain types and number tokens
+    # random.shuffle(board.terrain_array)
+    random.shuffle(board.probability_array)
+
+    # step 1: place hexagons
+    # req - ensure adjacent hexagon's aren't of similar type.
+
+    # if (central_desert == True):
+        # board.centres.remove(9)
+        # board.board[91] = 'X'
+    place_and_check(0)
+    for i in range(1,3):
+        place_and_check(i, 4)
+    place_and_check(3, 30)
+    for i in range(4,6):
+        place_and_check(i, 4, 30, 34)
+    place_and_check(6, 4, 34)
+    place_and_check(7, 38)
+    for i in range(8,11):
+        place_and_check(i, 4, 42, 38)
+    place_and_check(11, 4, 42)
+    place_and_check(12, 42, 38)
+    for i in range(13,16):
+        place_and_check(i, 4, 42, 38)
+    place_and_check(16, 34, 30)
+    for i in range(17,19):
+        place_and_check(i, 4, 34, 30)
+
+        # if value is equal to either of it's adjacents, select a new value
+        # if value is not equal to either of it's adjacents, place value
+
+    # step 2: place number tokens
+    # req - ensure adjacent hexagon's don't have the same number token
+    # req - tile intersection prob total cannot be higher than 11
+    # req - probabilities should be balanced over a resource
+    board.display_board()
+    return board
 
 setup_game()

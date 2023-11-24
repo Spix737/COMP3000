f415d0e5886ed8ac71e2d0c9f7312c7dae0ffb84
Spyros Babalitis <spyridon.babalitis@students.plymouth.ac.uk>
2023-11-24 00:59:02 +0000

    Board obtained (almost complete)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+import re
 from boardSetupClass import Board 
 from PlayerClass import Player
 from GameLogic import roll_dice
@@ -39,40 +41,122 @@ def setup_players():
 
 def setup_board():
     board = Board()
-    
-    kb = IDP.from_file("catan_board_idp_theory.idp")
-    T, S = kb.get_blocks("T, S")
-    for model in model_expand(T,S, max=1):
-        print(model)
-
-    
-
-    # Default Catan layout
-    tile_colors = {'hills': '#fa914f', 'forest': '#187b13', 'pasture': '#5ee557', 'mountains': '#9d998f', 'fields': '#fbf250', 'desert': '#fcf799', 'none': 'white'}
-    default_tiles = ['mountains', 'pasture', 'forest', 'fields', 'hills', 'pasture', 'hills', 'fields', 'forest', 'desert', 'forest', 'mountains', 'forest', 'mountains', 'fields', 'pasture', 'hills', 'fields', 'pasture']
-    default_tokens = [10, 2, 9, 12, 6, 4, 10, 9, 11, 7, 3, 8, 8, 3, 4, 5, 5, 6, 11]
+    # default_tiles = ['mountains', 'pasture', 'forest', 'fields', 'hills', 'pasture', 'hills', 'fields', 'forest', 'desert', 'forest', 'mountains', 'forest', 'mountains', 'fields', 'pasture', 'hills', 'fields', 'pasture']
+    # default_tokens = [10, 2, 9, 12, 6, 4, 10, 9, 11, 7, 3, 8, 8, 3, 4, 5, 5, 6, 11]
     tiles = ['none'] * 19
     tokens = [None] * 19
     neg_tiles = [[] for _ in range(19)]
     neg_tokens = [[] for _ in range(19)]
     next_tile = {'none': 'hills', 'hills': 'forest', 'forest': 'mountains', 'mountains': 'fields', 'fields': 'pasture', 'pasture': 'desert', 'desert': 'none'}
-    coordinates = [[0, -2], [1, -2], [2, -2], [-1, -1], [0, -1], [1, -1], [2, -1], [-2, 0], [-1, 0], [0, 0], [1, 0], [2, 0], [-2, 1], [-1, 1], [0, 1], [1, 1], [-2, 2], [-1, 2], [0, 2]]
-    highlight_tile = None
+    # coordinates = [[0, -2], [1, -2], [2, -2], [-1, -1], [0, -1], [1, -1], [2, -1], [-2, 0], [-1, 0], [0, 0], [1, 0], [2, 0], [-2, 1], [-1, 1], [0, 1], [1, 1], [-2, 2], [-1, 2], [0, 2]]
     extra_constraints = {'centerdesert': False, 'neighbourtoken': False, 'neighbourtile': False, 'balanceprob': False, 'elevenpips': False}
 
-    def reset():
-        for i in range(len(tiles)):
-            tiles[i] = 'none'
-            tokens[i] = None
-            neg_tiles[i] = []
-            neg_tokens[i] = []
+    kb = IDP.from_file("catan_board_idp_theory.idp")
+    T, S = kb.get_blocks("T, S")
+    for model in model_expand(T,S, max=1):
+        # print(model)
 
-    # Initiating the model expansion and drawing the initial board.
-    model_expand()
+        # Split the model into categories
+        categories = re.split(r"(\w+ := .+?})", model)
+        del categories[0]     
+        tile_type = categories[0]
+        tile_token = categories[2]
+        pattern = r"\((-?\d+), (-?\d+)\) -> (\w+)"
+        # Create a dictionary to store the results
+        matches = re.findall(pattern, tile_type)
+        coordinate_tile_dict = {(int(x), int(y)): tile_type for x, y, tile_type in matches}
+        matches = re.findall(pattern, tile_token)
+        coordinate_token_dict = {(int(x), int(y)): tile_token for x, y, tile_token in matches}
 
+        # Print the results
+        for coordinates, tile_type in coordinate_tile_dict.items():
+            q = coordinates[0]
+            r = coordinates[1]
+            idx = axialCoordToTileId(q, r)
+            if idx != None:
+              tiles[idx] = tile_type
+        for coordinates, tile_token in coordinate_token_dict.items():
+            q = coordinates[0]
+            r = coordinates[1]
+            idx = axialCoordToTileId(q, r)
+            if idx != None:
+              tokens[idx] = tile_token
+
+    print('tiles:')
     print(tiles)
+    print('tokens:')
+    print(tokens)
+
+# ADD THIS INFO TO OUR ARRAY
+
+def axialCoordToTileId(q, r):
+  # If invalid, return null.
+  if abs(q + r) > 2:
+    return None
+  idx = None
+  if (r == -2):
+    idx = abs(q)
+  elif (r == -1): 
+    idx = q + 4
+  elif (r == 0): 
+    idx = q + 9
+  elif (r == 1): 
+    idx = q + 14
+  elif (r == 2): 
+    idx = q + 18
+  return idx
+
+
+def setNeg(negDict, neg_tiles, neg_tokens):
+  for i in negDict['tile_type']:
+    coords = i
+    coords = coords.replace(')', '').replace('(', '').replace("'", '')
+    coords = coords.split(', ')
+    q = int(coords[0])
+    r = int(coords[1])
+    idx = axialCoordToTileId(q, r)
+    neg_tiles[idx] = int(negDict['tile_type'][i])
+    # console.log(negDict['tile_token'])
+  for j in negDict['tile_token']:
+    coords = j
+    coords = coords.replace(')', '').replace('(', '').replace("'", '')
+    coords = coords.split(', ')
+    q = int(coords[0])
+    r = int(coords[1])
+    idx = axialCoordToTileId(q, r)
+    neg_tokens[idx] = int(negDict['tile_token'][i])
+
+def setPos(posDict, tiles, tokens):
+  for i in posDict['tile_type']:
+    coords = i
+    coords = coords.replace(')', '').replace('(', '').replace("'", '')
+    coords = coords.split(', ')
+    q = int(coords[0])
+    r = int(coords[1])
+    idx = axialCoordToTileId(q, r)
+    if idx == None:
+      continue
+    tiles[idx] = posDict['tile_type'][i]
+    # console.log(posDict['tile_token'])
+  for j in posDict['tile_token']:
+    coords = j
+    coords = coords.replace(')', '').replace('(', '').replace("'", '')
+    coords = coords.split(', ')
+    q = int(coords[0])
+    r = int(coords[1])
+    idx = axialCoordToTileId(q, r)
+    if idx == None:
+      continue
+    tokens[idx] = int(posDict['tile_token'][i])
+
+  def reset(neg_tiles, neg_tokens):
+      for i in range(len(tiles)):
+          tiles[i] = 'none'
+          tokens[i] = None
+          neg_tiles[i] = []
+          neg_tokens[i] = []
 
     # board.display_board()
-    return board
+
 
 setup_game()
